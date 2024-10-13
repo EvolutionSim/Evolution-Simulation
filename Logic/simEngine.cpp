f5104ae5a71b1629e8d74125300a9e1149d94960
Edwin Tan <edwintan7962@gmail.com>
2024-10-12 21:37:58 -0500

    setup
    Created basic environment and creature objects, waiting for environment and creature classes to be finished to complete the initialization. Also, the environment files were titled "enviroment" so I changed them.

---
+++ Logic/simEngine.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "environment.h"
+#include "creature.h"
+
+const int NUM_CREATURE = 1;
+
+int main() {
+    Environment env;
+    Creature arr_creature[NUM_CREATURE];
+}
