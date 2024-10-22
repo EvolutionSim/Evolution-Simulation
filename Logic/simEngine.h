8467b8dac4aa8407d8eb43e7c7a189443986a609
JP Roque <juanproque@outlook.com>
2024-10-22 16:41:09 -0500

    Added breed function and a few includes to other files
    Added a breed function that creates a child using the average of the parents stats plus a chance to mutate. Also made sure every cpp file included its header, every header file had a header guard, and fixed a typo in the name of the environment files.

--- Logic/simEngine.h
+++ Logic/simEngine.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <vector>
-#include "pose2d.h"
+#include "Math/pose2d.h"
 #include "creature.h"
+#include "environment.h"
 
 class SimEngine{
     protected:
@@ -11,15 +12,20 @@ class SimEngine{
         float size_{};
         int creatureCount_{};
         int foodCount_{};
-        std::vector<Creature> creatureList;
+        float mutationChance_{};
+        float mutationIntensity_{};
+        Environment environment;
+        std::vector<Creature> creatureList_;
     public:
-        SimEngine(float timeStep, float dayLength, int days, float size, int creatureCount_, int foodCount): 
+        SimEngine(float timeStep, float dayLength, int days, float size, int creatureCount_, int foodCount, float mutationChance, float mutationIntensity): 
         timeStep_{timeStep}, dayLength_{dayLength}, days_{days}, size_(size), 
-        creatureCount_{creatureCount_}, foodCount_{foodCount} {};
+        creatureCount_{creatureCount_}, foodCount_{foodCount}, mutationChance_{mutationChance}, mutationIntensity_{mutationIntensity} {};
 
         void init();
         void step();
         void simulate();
         Pose2D locateFood(int, Pose2D);
-        void endDay();
+        float generateMutation();
+        void breed(Creature& p1, Creature& p2);
+        void endOfDay();
 };
